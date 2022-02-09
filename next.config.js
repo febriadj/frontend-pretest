c5a407fa9937a7adebbc4e01f4115a2be555a2c7
febriadj <iamfebriadji@gmail.com>
2022-02-09 13:18:51 +0700

    :wrench: Update next.js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["m.media-amazon.com"],
-  }
+    domains: ['m.media-amazon.com'],
+  },
+  env: {
+    OMDB_API_KEY: 'http://www.omdbapi.com/?apikey=b3107253',
+  },
 }
